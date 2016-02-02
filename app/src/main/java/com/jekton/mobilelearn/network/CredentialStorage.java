5b957a2f6ad511a9c267845555fd034b56e4ea0d
Jekton <ljtong64@outlook.com>
2016-02-02 19:14:14 +0800

    Add a CredentialStorage to store user email and password in SharedPreferences

---
+++ app/src/main/java/com/jekton/mobilelearn/network/CredentialStorage.java
@@ -0,0 +1,50 @@
+package com.jekton.mobilelearn.network;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.jekton.mobilelearn.MyApplication;
+
+/**
+ * @author Jekton
+ */
+class CredentialStorage {
+
+    private static final String SP_CREDENTIAL_NAME = "SP_CREDENTIAL_NAME";
+    private static final String CREDENTIAL_KEY_EMAIL = "CREDENTIAL_KEY_EMAIL";
+    private static final String CREDENTIAL_KEY_PASSWORD = "CREDENTIAL_KEY_PASSWORD";
+
+    private static SharedPreferences sPreferences;
+
+    static {
+        sPreferences = MyApplication.getInstance()
+                .getSharedPreferences(SP_CREDENTIAL_NAME,
+                                      Context.MODE_PRIVATE);
+    }
+
+
+    public static void storeCredential(String email, String password) {
+        SharedPreferences.Editor editor = sPreferences.edit();
+        editor.putString(CREDENTIAL_KEY_EMAIL, email);
+        editor.putString(CREDENTIAL_KEY_PASSWORD, password);
+        editor.apply();
+    }
+
+    /**
+     * @return a String[] of email and password
+     */
+    public static String[] getCredential() {
+        String email = sPreferences.getString(CREDENTIAL_KEY_EMAIL, "");
+        String password = sPreferences.getString(CREDENTIAL_KEY_PASSWORD, "");
+
+        return new String[] {
+                email,
+                password
+        };
+    }
+
+
+
+    private CredentialStorage() {
+    }
+}
