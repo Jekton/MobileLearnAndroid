b4406a67c06c72c5868bc50a63a1db96510c6738
Jekton <ljtong64@outlook.com>
2016-02-15 20:58:01 +0800

    Implement CourseDetailsDocument

--- app/src/main/java/com/jekton/mobilelearn/course/CourseDetailsDocument.java
+++ app/src/main/java/com/jekton/mobilelearn/course/CourseDetailsDocument.java
@@ -1,16 +1,49 @@
 package com.jekton.mobilelearn.course;
 
-import com.jekton.mobilelearn.common.dv.network.SimpleHttpDocument;
+import android.support.annotation.NonNull;
 
+import com.jekton.mobilelearn.common.dv.network.SimpleHttpDocument;
+import com.jekton.mobilelearn.common.network.AbstractHttpRunnable;
+import com.jekton.mobilelearn.common.network.OnResponseCallback;
+import com.jekton.mobilelearn.network.UrlConstants;
+
+import okhttp3.Request;
 import okhttp3.Response;
 
 /**
  * @author Jekton
  */
-class CourseDetailsDocument extends SimpleHttpDocument<CourseDetailsViewOps> {
+class CourseDetailsDocument extends SimpleHttpDocument<CourseDetailsViewOps>
+        implements CourseDetailsDocumentOps {
+
+    @Override
+    public void onTakeCourse(String courseId) {
+        doHttpRequest(new TakeCourseRunnable(courseId, this));
+    }
 
     @Override
     public void onResponseSuccess(Response response) {
+        CourseDetailsViewOps view = getView();
+        if (view != null) {
+            view.onCourseTaken();
+        }
+    }
 
+    private class TakeCourseRunnable extends AbstractHttpRunnable {
+
+        private final String mCourseId;
+
+        public TakeCourseRunnable(String courseId, OnResponseCallback callback) {
+            super(callback);
+            mCourseId = courseId;
+        }
+
+        @NonNull
+        @Override
+        protected Request makeRequest() {
+            return new Request.Builder()
+                    .url(String.format(UrlConstants.TAKE_COURSE_TEMPLATE, mCourseId))
+                    .build();
+        }
     }
 }
--- app/src/main/java/com/jekton/mobilelearn/network/UrlConstants.java
+++ app/src/main/java/com/jekton/mobilelearn/network/UrlConstants.java
@@ -12,4 +12,6 @@ public class UrlConstants {
 
     public static final String GET_TAKEN_COURSES = HOST + "/api/takencourses";
     public static final String GET_ALL_COURSES = HOST + "/api/allcourses";
+    public static final String TAKE_COURSE_TEMPLATE = HOST + "/api/takecourse/%s";
+
 }
