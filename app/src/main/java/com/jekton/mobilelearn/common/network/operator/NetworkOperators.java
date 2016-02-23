ab63361b1992777f25652abd109785fced929da8
Jekton <ljtong64@outlook.com>
2016-02-23 16:06:34 +0800

    Add two static factory method to NetworkOperators to provide app-shared NetworkOperator instance
    Since we can't rely the client to shutdown each NetworkOperator instance once it's unneeded, we do have to used this way

--- app/src/main/java/com/jekton/mobilelearn/common/network/operator/NetworkOperators.java
+++ app/src/main/java/com/jekton/mobilelearn/common/network/operator/NetworkOperators.java
@@ -3,12 +3,36 @@ package com.jekton.mobilelearn.common.network.operator;
 import java.util.HashMap;
 
 /**
+ * Util class that used to provide some static factory method to generate instance of
+ * {@link NetworkOperator}.
+ *
+ * For common use case, client should use {@link #getSingleRequestOperator()} and
+ * {@link #getMultiRequestOperator()} to used a app-shared instance.
+ *
  * Note: We DO NOT want to provide static factory methods that takes an instance of
  * {@link java.util.concurrent.Executor} since it's the implementation details of our framework.
  *
  * @author Jekton
  */
 public class NetworkOperators {
+
+    private static class SingleRequestSingleton {
+        public static final NetworkOperator sNetworkOperator = newSingleRequestOperator();
+    }
+
+    private static class MultiRequestSingleton {
+        public static final NetworkOperator sNetworkOperator = newMultiRequestOperator();
+    }
+
+
+    public static NetworkOperator getSingleRequestOperator() {
+        return SingleRequestSingleton.sNetworkOperator;
+    }
+
+    public static NetworkOperator getMultiRequestOperator() {
+        return MultiRequestSingleton.sNetworkOperator;
+    }
+
 
     /**
      * @return a {@link NetworkOperator} that can just handle {@link okhttp3.Request} exactly one
