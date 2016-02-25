83edc4417cf834a634e83441b49f4829d9ba0b4c
Jekton <ljtong64@outlook.com>
2016-02-25 15:20:23 +0800

    Implement the document for FileActivity

--- app/src/main/java/com/jekton/mobilelearn/course/file/FileActivityDocument.java
+++ app/src/main/java/com/jekton/mobilelearn/course/file/FileActivityDocument.java
@@ -1,6 +1,19 @@
 package com.jekton.mobilelearn.course.file;
 
+import com.google.gson.Gson;
 import com.jekton.mobilelearn.common.dv.AbstractDocument;
+import com.jekton.mobilelearn.common.network.HttpUtils;
+import com.jekton.mobilelearn.common.network.operator.NetworkOperator;
+import com.jekton.mobilelearn.common.network.operator.NetworkOperators;
+import com.jekton.mobilelearn.common.network.operator.OnResponseCallback;
+import com.jekton.mobilelearn.common.util.Logger;
+import com.jekton.mobilelearn.course.Course;
+import com.jekton.mobilelearn.network.UrlConstants;
+
+import java.io.IOException;
+
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * @author Jekton
@@ -8,9 +21,48 @@ import com.jekton.mobilelearn.common.dv.AbstractDocument;
 class FileActivityDocument extends AbstractDocument<FileActivityOps>
         implements FileActivityDocumentOps {
 
+    private static final String LOG_TAG = FileActivityDocument.class.getSimpleName();
+
+    private NetworkOperator mNetworkOperator = NetworkOperators.getSingleRequestOperator();
+    private volatile String mCourseId;
+
+
 
     @Override
-    public void initFileList() {
+    public void initFileList(String courseId) {
+        String url = String.format(UrlConstants.GET_TAKEN_COURSE_TEMPLATE, courseId);
+        Request request = HttpUtils.makeGetRequest(url);
+        mNetworkOperator.executeRequest(request, new OnResponseCallback() {
+            @Override
+            public void onResponseSuccess(Response response) {
+                FileActivityOps view = getView();
+                if (view != null) {
+                    try {
+                        Gson gson = new Gson();
+                        Course course = gson.fromJson(response.body().string(), Course.class);
+                        view.onFilesChange(FileUtil.makeFileList(course));
+                    } catch (IOException e) {
+                        Logger.e(LOG_TAG, e);
+                    }
+                }
+            }
+
+            @Override
+            public void onNetworkFail() {
+                FileActivityOps view = getView();
+                if (view != null) {
+                    view.onNetworkError();
+                }
+            }
+
+            @Override
+            public void onResponseFail(Response response) {
+                FileActivityOps view = getView();
+                if (view != null) {
+                    view.onGetFileListFail();
+                }
+            }
+        });
 
     }
 
---
+++ app/src/main/java/com/jekton/mobilelearn/course/file/FileUtil.java
@@ -0,0 +1,25 @@
+package com.jekton.mobilelearn.course.file;
+
+import android.support.annotation.NonNull;
+
+import com.jekton.mobilelearn.course.Course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Jekton
+ */
+class FileUtil {
+
+    public static @NonNull List<CourseFile> makeFileList(Course course) {
+        List<CourseFile> courseFiles = new ArrayList<>();
+
+
+
+        return courseFiles;
+    }
+
+    private FileUtil() {
+    }
+}
