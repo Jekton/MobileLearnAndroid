28089c0685e78c43374244150bfd50bcbe4e793a
Jekton <ljtong64@outlook.com>
2016-02-25 14:54:37 +0800

    Implement the FileListAdapter

--- app/src/main/java/com/jekton/mobilelearn/course/file/CourseFile.java
+++ app/src/main/java/com/jekton/mobilelearn/course/file/CourseFile.java
@@ -1,6 +1,9 @@
 package com.jekton.mobilelearn.course.file;
 
 import android.support.annotation.IntDef;
+import android.support.annotation.StringRes;
+
+import com.jekton.mobilelearn.R;
 
 /**
  * @author Jekton
@@ -17,4 +20,18 @@ public class CourseFile {
     public @FileState int state;
     public String filename;
     public String path;
+
+
+    public @StringRes int getStateStringRes() {
+        switch (state) {
+            case STATE_NOT_DOWNLOAD:
+                return R.string.file_state_not_download;
+            case STATE_DOWNLOADING:
+                return R.string.file_state_downloading;
+            case STATE_DOWNLOADED:
+                return R.string.file_state_downloaded;
+            default:
+                throw new AssertionError("unknown state " + state);
+        }
+    }
 }
---
+++ app/src/main/java/com/jekton/mobilelearn/course/file/FileListAdapter.java
@@ -0,0 +1,85 @@
+package com.jekton.mobilelearn.course.file;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.jekton.mobilelearn.R;
+
+import java.util.List;
+
+/**
+ * @author Jekton
+ */
+class FileListAdapter extends BaseAdapter implements View.OnClickListener {
+
+    private Context mContext;
+    private OnButtonClicked mOnButtonClicked;
+    private List<CourseFile> mCourseFiles;
+
+    public FileListAdapter(Context context, OnButtonClicked onButtonClicked) {
+        mContext = context;
+        mOnButtonClicked = onButtonClicked;
+    }
+
+    @Override
+    public int getCount() {
+        return mCourseFiles == null ? 0 : mCourseFiles.size();
+    }
+
+    @Override
+    public CourseFile getItem(int position) {
+        return mCourseFiles.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext)
+                    .inflate(R.layout.widget_file_list_item, parent, false);
+            ViewHolder holder = new ViewHolder();
+            holder.mTextView = (TextView) convertView.findViewById(R.id.filename);
+            holder.mButton = (Button) convertView.findViewById(R.id.button);
+            holder.mButton.setOnClickListener(this);
+            convertView.setTag(holder);
+        }
+
+        ViewHolder holder = (ViewHolder) convertView.getTag();
+        CourseFile file = getItem(position);
+        holder.mTextView.setText(file.filename);
+        holder.mButton.setText(file.getStateStringRes());
+        // use to find that which item clicked
+        holder.mButton.setTag(position);
+        return convertView;
+    }
+
+    public void updateCourseFiles(List<CourseFile> courseFiles) {
+        mCourseFiles = courseFiles;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int position = (Integer) v.getTag();
+        mOnButtonClicked.onButtonClicked(position);
+    }
+
+
+    private static class ViewHolder {
+        TextView mTextView;
+        Button mButton;
+    }
+
+    public interface OnButtonClicked {
+        void onButtonClicked(int position);
+    }
+}
